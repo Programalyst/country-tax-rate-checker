0e726763a5b543778165d06736a1555eb0bc3acd
Leonard <leonard@gamenami.com>
2021-02-07 18:03:55 +0800

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from './components/NavBar';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import About from './pages/About';
+import TaxRates from './pages/TaxRates';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path='/' exact component = {TaxRates} />
+          <Route path='/about'component={About} />
+          <Route path='/taxRates' component ={TaxRates} />
+        </Switch>
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
