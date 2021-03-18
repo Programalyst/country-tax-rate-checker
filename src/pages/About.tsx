d3e9366ba22df3af4d84316087686d6df4a25baa
Leonard <leonard@gamenami.com>
2021-03-18 16:30:43 +0800

    Added AutoComplete component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About: React.FC = () => {
 
     return (
-        <div>
+        <div className="about">
             <h1>About Page</h1>
         </div>
     )
--- src/pages/TaxRates.tsx
+++ src/pages/TaxRates.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import AutoComplete from '../components/AutoComplete/AutoComplete';
 import CountryDropdownList from '../components/CountryDropdownList'
+import CountryList from '../stores/CountryList';
 
 const TaxRates: React.FC = () => {
 
@@ -7,6 +9,7 @@ const TaxRates: React.FC = () => {
         <div>
             <h1>Corporate Tax Rates and Tax Treaties</h1>
             <CountryDropdownList />
+            <AutoComplete itemsList={CountryList}/>
         </div>
     )
 
